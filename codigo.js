fb73031868e319fcc2f8cfee9d103b077ea2696d
Bruno Costa <62752426+Bruno-Costa-fig@users.noreply.github.com>
2022-09-10 15:51:02 -0300

    first commit

---
+++ codigo.js
@@ -0,0 +1,37 @@
+/*
+  Tipagem dinâmica
+
+  Tipos de dados:
+    # string - palavra ou caractere
+    # number - inteiro e decimal
+    # boolean - verdadeiro ou falso (true ou false)
+    # object - objeto
+    # undefined - null
+
+  Tipos de variaveis:
+    # var - não é obrigatório inicializar
+    # let - é obrigatório inicializar
+    # const - não permite alterar o tipo após ser inicializado
+*/
+
+// Atribuição é sempre com o '='
+// = -> recebe
+// == -> igual
+
+
+// Operadores Aritméticos
+//     - Adição (+)
+var soma = 5 + 2
+console.log(soma)
+//     - subtração (-)
+let sub = 5 - 2
+console.log(sub)
+//     - Multiplicação (*)
+let mult = 5 * 2
+console.log(mult)
+//     - Divisão (/)
+let div = 5 / 2
+console.log(div)
+//     - Módulo (%)
+let mod = 6 % 2
+console.log(mod)
---
+++ passos_introducao.md
@@ -0,0 +1,35 @@
+## O que é e para que serve?
+  - História
+    - Criada em 1995 por JavaScript Brendan Eich;
+    - Não tem NADA a ver com Java;
+    - Tornou-se ECMA em 1997;
+    - ECMA-262 é o nome do padrão e ECMAScript é o nome oficial;
+    - possui várias versões*
+
+  - Exemplos de uso
+
+## Como usar + exemplo
+  - Tipos de script
+  - exemplo
+    Fonte: ["https://www.w3schools.com/js/tryit.asp?filename=tryjs_intro_lightbulb"]
+
+## Declaração de variáveis
+  - Uso de variáveis (var, let, const)
+  - Extensão coderun
+  - Operadores Aritméticos
+    - Adição
+    - subtração
+    - Multiplicação
+    - Divisão
+    - Módulo
+
+    - Incremento
+      - pré
+      - pós
+    - Decremento
+      - pré
+      - pós
+
+    - Precedência do ()
+
+## Subir código no GitHub
